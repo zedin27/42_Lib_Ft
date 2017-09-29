0d32869bfe7fbc1ebfd758a3b373a4a21f2a05ad
Rubikhan <sammaddux@gmail.com>
2017-09-29 16:06:28 -0700

    ft_putnbr.c

---
+++ ft_putnbr.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+void ft_putnbr(int n)
+{
+	int a;
+
+	if (n > 2147483647 || n < -2147483647)
+	{
+		write(1, "error: exceeds max signed int value", 35);
+		return;
+	}
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = (n * -1);
+	}
+	if (n > 9)
+	{
+		ft_putnbr(n/10);
+	}
+	a = (n % 10) + '0';
+	write(1, &a, 1);
+}
