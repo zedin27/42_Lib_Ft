cc0232a105c68663e204703cadde3f5ef99e311f
Rubikhan <sammaddux@gmail.com>
2017-09-29 17:02:50 -0700

    ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char* ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t a;
+	size_t b;
+	a = 0;
+	b = ft_strlen(little);
+
+	if (*little == '\0')
+		return((char*)big);
+
+	while (*big != '\0' && a < len)
+	{
+		if (ft_strncmp(big, little, b) == 0)
+			return ((char*)big);
+		a++;
+		big++;
+	}
+	return(NULL);
+}
