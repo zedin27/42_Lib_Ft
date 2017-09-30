dd9f313e9649c2bd96279d9a373915b07e286ae0
Rubikhan <sammaddux@gmail.com>
2017-09-29 17:17:19 -0700

    added commented entry of ft_memdel for organizational purposes

--- libft.h
+++ libft.h
@@ -18,6 +18,7 @@ void* ft_memccpy(void* dst, const void* src, int c, size_t n);
 void* ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
 void* ft_memcpy(void* dst, const void* src, size_t n);
+//		ft_memdel
 void* ft_memmove(void* dst, const void *src, size_t len);
 void *ft_memset(void *b, int c, size_t len);
 void ft_putchar(char c);
