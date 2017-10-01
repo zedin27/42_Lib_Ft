9761d3ceed0e8acf818c04171e43b5209ca5439c
Rubikhan <sammaddux@gmail.com>
2017-10-01 15:54:58 -0700

    cast return values of strcmp and strncmp implementations as unsigned chars

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -2,6 +2,7 @@
 
 int ft_strcmp(const char *s1, const char *s2)
 {
+
 	while (*s1 == *s2 && (*s1 != '\0' || *s2 != '\0'))
 	{
 		s1++;
@@ -9,7 +10,7 @@ int ft_strcmp(const char *s1, const char *s2)
 	}
 	if (*s1 != *s2)
 	{
-		return((*s1 - *s2));
+		return((unsigned char)*s1 - (unsigned char)*s2);
 	}
 
 	else
