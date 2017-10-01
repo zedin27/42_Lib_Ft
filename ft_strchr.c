c0d7195cc1470f805695b53084ca51f05bb931b0
Rubikhan <sammaddux@gmail.com>
2017-10-01 16:18:16 -0700

    restructured strchr implementation to work with \0

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,21 +1,39 @@
 #include "libft.h"
 
+/* char* ft_strchr(const char *s, int c) */
+/* { */
+/* 	size_t a; */
+/* 	size_t b; */
+/* 	char* z; */
+
+/* 	a = ft_strlen(s); */
+/* 	b = 0; */
+/* 	z = (char*)s; */
+
+/* 	while(b < a) */
+/* 	{ */
+/* 		if (*z == (char)c) */
+/* 			return(z); */
+/* 		z++; */
+/* 		b++; */
+/* 	} */
+/* 	return(NULL); */
+/* } */
+
 char* ft_strchr(const char *s, int c)
 {
-	size_t a;
-	size_t b;
 	char* z;
 
-	a = ft_strlen(s);
-	b = 0;
 	z = (char*)s;
 
-	while(b < a)
+	if (*z == (char)c)
+		return(z);
+
+	while(*z)
 	{
+		z++;
 		if (*z == (char)c)
 			return(z);
-		z++;
-		b++;
 	}
 	return(NULL);
 }
