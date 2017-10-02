9029a410aafa3dc008ad4d77d2027cabccb7705c
Rubikhan <sammaddux@gmail.com>
2017-10-01 18:20:37 -0700

    added commented duplicate of ft_putnbr

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -2,8 +2,7 @@
 
 void ft_putnbr(int n)
 {
-	int a;
-
+	
 	if (n > 2147483647 || n < -2147483647)
 	{
 		write(1, "error: exceeds max signed int value", 35);
@@ -18,6 +17,28 @@ void ft_putnbr(int n)
 	{
 		ft_putnbr(n/10);
 	}
-	a = (n % 10) + '0';
-	write(1, &a, 1);
+	n = (n % 10) + '0';
+	write(1, &n, 1);
 }
+
+/* void ft_putnbr(int n) */
+/* { */
+/* 	int a; */
+
+/* 	if (n > 2147483647 || n < -2147483647) */
+/* 	{ */
+/* 		write(1, "error: exceeds max signed int value", 35); */
+/* 		return; */
+/* 	} */
+/* 	if (n < 0) */
+/* 	{ */
+/* 		ft_putchar('-'); */
+/* 		n = (n * -1); */
+/* 	} */
+/* 	if (n > 9) */
+/* 	{ */
+/* 		ft_putnbr(n/10); */
+/* 	} */
+/* 	a = (n % 10) + '0'; */
+/* 	write(1, &a, 1); */
+/* } */
