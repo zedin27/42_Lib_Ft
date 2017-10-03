1efdbf92e45f5cad2238c3c5121f42e00960673b
Rubikhan <sammaddux@gmail.com>
2017-10-02 21:59:54 -0700

    accounted for len and strlen(little) being the same value in strnstr implementation

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,10 +10,20 @@ char* ft_strnstr(const char *big, const char *little, size_t len)
 	if (*little == '\0')
 		return((char*)big);
 
-	while (*big != '\0' && a < len)
+	if(b == len)
 	{
 		if (ft_strncmp(big, little, b) == 0)
+			return((char*)big);
+		else
+			return(NULL);
+	}
+
+	while (*big != '\0' && (a < len))
+	{
+		if (ft_strncmp(big, little, b) == 0)
+		{
 			return ((char*)big);
+		}
 		a++;
 		big++;
 	}
