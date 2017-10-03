5a80d1499658f6616776433c9d955148e527bd1a
Rubikhan <sammaddux@gmail.com>
2017-10-02 20:14:05 -0700

    when provided len argument is longer than strlen(src) my strncpy now correctly pads dst with trailing \0s

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -1,13 +1,27 @@
 #include "libft.h"
 
+/*
+** the ( len - 1 ) is crucial in both places
+*/
+
 char* ft_strncpy(char* dst, const char* src, size_t len)
 {
-
+	char* save;
 	size_t a;
+	size_t b;
+	
+	b = ft_strlen(src);
 	a = 0;
+	save = dst;
 
-    while (((*dst++ = *src++) && a < (len - 1) ))
+
+    while (((*save++ = *src++) && a < (len - 1) ))
 		a++;
+
+	while (b++ < (len - 1))
+		*save++ = '\0';
 
     return (dst);
 }
+
+
