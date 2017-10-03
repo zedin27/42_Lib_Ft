11e9050cec12286a5dc9645b4605bb6791f0a206
Rubikhan <sammaddux@gmail.com>
2017-10-03 15:32:13 -0700

    memmove implementation now works thanks to slack

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,23 +1,35 @@
 #include "libft.h"
 
-
+/*
+** I have no idea how that if statement works
+** I just searched slack and found it
+*/
 
 void* ft_memmove(void* dst, const void *src, size_t len)
 {
+	int e;
 	const char* a;
 	char* d;
 
 	d = (char*)dst;
-	a = src;
+	a = (char*)src;
+	e = 0;
 
+	if (dst < src) 
+		while (e < (int)len)
+		{
+			d[e] = a[e];
+			e++;
+		}
+	
+	else
 	while ((int)len > 0)
 	{
 		len--;
-		*d = *a;
-		d++;
-		a++;
+		d[len] = a[len];
 	}
 
 	return (dst);
 
 }
+
