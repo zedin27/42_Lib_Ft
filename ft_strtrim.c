05cb84be6d1391c78e3b7240ce1571cb5baf2b76
Rubikhan <sammaddux@gmail.com>
2017-10-13 02:58:56 -0700

    formatted ft_strtrim.c to pass the norm

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,36 +6,36 @@
 /*   By: smaddux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:45:56 by smaddux           #+#    #+#             */
-/*   Updated: 2017/10/13 02:47:38 by smaddux          ###   ########.fr       */
+/*   Updated: 2017/10/13 02:57:03 by smaddux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
- char    *ft_strtrim(char const *s) 
- { 
-      size_t        i;  
-      size_t        begin;  
-      size_t        terminus;  
-      char        	*str;  
+char	*ft_strtrim(char const *s)
+{
+	size_t	i;
+	size_t	begin;
+	size_t	terminus;
+	char	*str;
 
-      if (s == NULL)  
-          return (NULL);  
-      i = 0;  
-      while (s[i] && (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'))  
-          i++;  
-      if (!s[i])  
-          return (ft_strnew(0));  
-      begin = i;  
-      while (s[i])  
-          i++;  
-      i--;  
-      while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')  
-          i--;  
-      terminus = i + 1;  
-      str = ft_strnew(terminus - begin);  
-      if (str == NULL)  
-          return (NULL);  
-      ft_strncpy(str, &s[begin], terminus - begin);  
-      return (str);  
- } 
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] && (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'))
+		i++;
+	if (!s[i])
+		return (ft_strnew(0));
+	begin = i;
+	while (s[i])
+		i++;
+	i--;
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		i--;
+	terminus = i + 1;
+	str = ft_strnew(terminus - begin);
+	if (str == NULL)
+		return (NULL);
+	ft_strncpy(str, &s[begin], terminus - begin);
+	return (str);
+}
