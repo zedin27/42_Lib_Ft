f6ad32199eb1455aa18f4515cce1886f5be5ed7b
Rubikhan <sammaddux@gmail.com>
2017-10-13 01:22:55 -0700

    t

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: smaddux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:37:15 by smaddux           #+#    #+#             */
-/*   Updated: 2017/10/12 21:27:21 by smaddux          ###   ########.fr       */
+/*   Updated: 2017/10/13 01:22:41 by smaddux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,42 +15,91 @@
 /*
 **y0 I need a word count function fml
 */
-
-char	**ft_strsplit(char const *s, char c)
+static char    *ft_strsep(char const *s, int len, long i)
 {
-	int		a;
-	int		i;
-	int		z;
-	char	*y;
-	char	**b;
+    char    *str;
 
-	if (s == NULL)
-		return (NULL);
-	a = ft_delimitcount((char*)s, (int)c);
-	i = 0;
-	z = 0;
-	y = (char*)s;
-	b = malloc(sizeof(char*) * (a + 1));
-	if (b == NULL)
-		return (NULL);
-	while (i < (a) && *s)
-	{
-		while (*s != c)
-		{
-			z++;
-			s++;
-		}
-		if (z != 0)
-		{
-			b[i] = malloc((z) * sizeof(char));
-			b[i] = ft_strncpy(b[i], y, z);
-			y += z;
-			z = 0;
-			i++;
-		}
-		y++;
-		s++;
-	}
-	b[i] = 0;
-	return (b);
+    str = ft_strnew(len);
+    if (str == NULL)
+        return (NULL);
+    str[len--] = '\0';
+    while (len >= 0)
+        str[len--] = s[i--];
+    return (str);
 }
+
+static char    **ft_strsubsplit(char const *s, char **arr, int arr_len, char c)
+{
+    int        index;
+    int        str_len;
+    long    i;
+
+    i = 0;
+    index = 0;
+    while (index < arr_len)
+    {
+        str_len = 0;
+        while (s[i] && s[i] == c)
+            i++;
+        while (s[i] && s[i] != c)
+        {
+            i++;
+            str_len++;
+        }
+        if (s[i] == '\0' || s[i] == c)
+            arr[index++] = ft_strsep(s, str_len, i - 1);
+    }
+    arr[index] = NULL;
+    return (arr);
+}
+
+char        **ft_strsplit(char const *s, char c)
+{
+    int        arr_len;
+    char    **arr;
+
+    if (s == NULL)
+        return (NULL);
+    arr_len = ft_strcount(s, c);
+    if (!(arr = (char **)malloc(sizeof(char *) * arr_len + 1)))
+        return (NULL);
+    return (ft_strsubsplit(s, arr, arr_len, c));
+}
+/* char	**ft_strsplit(char const *s, char c) */
+/* { */
+/* 	int		a; */
+/* 	int		i; */
+/* 	int		z; */
+/* 	char	*y; */
+/* 	char	**b; */
+
+/* 	if (s == NULL) */
+/* 		return (NULL); */
+/* 	a = ft_delimitcount((char*)s, (int)c); */
+/* 	i = 0; */
+/* 	z = 0; */
+/* 	y = (char*)s; */
+/* 	b = malloc(sizeof(char*) * (a + 1)); */
+/* 	if (b == NULL) */
+/* 		return (NULL); */
+/* 	while (i < (a) && *s) */
+/* 	{ */
+/* 		while (*s != c) */
+/* 		{ */
+/* 			z++; */
+/* 			s++; */
+/* 		} */
+/* 		if (z != 0) */
+/* 		{ */
+/* 			b[i] = malloc((z) * sizeof(char)); */
+/* 			b[i] = ft_strncpy(b[i], y, z); */
+/* 			y += z; */
+/* 			z = 0; */
+/* 			i++; */
+/* 		} */
+/* 		y++; */
+/* 		s++; */
+/* 	} */
+/* 	b[i] = 0; */
+/* 	return (b); */
+/* } */
