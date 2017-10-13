ab76b81bd4f5cfd4911387b5c585b74bfd6ce287
Rubikhan <sammaddux@gmail.com>
2017-10-13 03:06:36 -0700

    reformatted ft_strsplit.c to adhere to the norm

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: smaddux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:37:15 by smaddux           #+#    #+#             */
-/*   Updated: 2017/10/13 02:33:55 by smaddux          ###   ########.fr       */
+/*   Updated: 2017/10/13 03:05:59 by smaddux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 **y0 I need a word count function fml
 */
 
-static char *ft_strshuffle(char const *s, int length, long c)
+static char	*ft_strshuffle(char const *s, int length, long c)
 {
-	char *a;
+	char	*a;
 
 	a = ft_strnew(length);
 	if (a == NULL)
@@ -26,44 +26,43 @@ static char *ft_strshuffle(char const *s, int length, long c)
 	a[length--] = '\0';
 	while (length >= 0)
 		a[length--] = s[c--];
-	return(a);
+	return (a);
 }
 
-static char **ft_strmicrosplit(char const *s, char **col, int col_len, char a)
+static char	**ft_strmicrosplit(char const *s, char **col, int col_len, char a)
 {
-	int	dex;
-	int	str_len;
-	long d;
+	int		dex;
+	int		str_len;
+	long	d;
 
 	d = 0;
 	dex = 0;
-
 	while (dex < col_len)
 	{
 		str_len = 0;
-			while (s[d] && s[d] == a)
-				d++;
-			while (s[d] && s[d] != a)
-			{
-				d++;
-				str_len++;
-			}
-			if (s[d] == '\0' || s[d] == a)
-				col[dex++] = ft_strshuffle(s, str_len, d - 1);
+		while (s[d] && s[d] == a)
+			d++;
+		while (s[d] && s[d] != a)
+		{
+			d++;
+			str_len++;
+		}
+		if (s[d] == '\0' || s[d] == a)
+			col[dex++] = ft_strshuffle(s, str_len, d - 1);
 	}
 	col[dex] = NULL;
 	return (col);
 }
 
-char	**ft_strsplit(char const *s, char c)
+char		**ft_strsplit(char const *s, char c)
 {
-	int col_len;
-	char **col;
-	
+	int		col_len;
+	char	**col;
+
 	if (s == NULL)
 		return (NULL);
 	col_len = ft_delimitcount((char*)s, c);
 	if (!(col = (char **)malloc(sizeof(char *) * col_len + 1)))
 		return (NULL);
-	return(ft_strmicrosplit(s, col, col_len, c));
+	return (ft_strmicrosplit(s, col, col_len, c));
 }
