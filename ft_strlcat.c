4b4c95bf02f0f03341d467f83489ee80159b8f88
Rubikhan <sammaddux@gmail.com>
2017-10-12 20:45:29 -0700

    rewrote ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,50 +6,31 @@
 /*   By: smaddux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:20:49 by smaddux           #+#    #+#             */
-/*   Updated: 2017/10/12 15:23:00 by smaddux          ###   ########.fr       */
+/*   Updated: 2017/10/12 20:45:00 by smaddux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
-** Two other ways to write the ternary code:
-**
-**  	while (pntsize-- != 0 && *dstp != '\0')
-**  		dstp++;
-**  	len = dstp - dst;
-**
-** ##############################################
-**
-**  	if (pntsize > ft_strlen(dstp))
-**  		len = ft_strlen(dstp);
-**  	else
-**  		len = size;
-*/
-
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	char		*dstp;
-	const char	*srcp;
-	size_t		pntsize;
-	size_t		len;
+	int a;
+	int s_size;
+	int d_size;
+	int space;
 
-	dstp = dst;
-	srcp = src;
-	pntsize = size;
-	(pntsize > ft_strlen(dstp)) ? (len = ft_strlen(dstp)) : (len = size);
-	dstp += len;
-	pntsize = size - len;
-	if (pntsize == 0)
-		return (len + ft_strlen(srcp));
-	while (*srcp != '\0')
-	{
-		if (pntsize-- != 1)
-		{
-			*dstp++ = *srcp;
-		}
-		srcp++;
-	}
-	*dstp = '\0';
-	return (len + ((srcp) - src));
+	s_size = ft_strlen(src);
+	d_size = 0;
+	space = size;
+
+	while (dst[d_size] != '\0' &&  space-- != 0)
+		d_size++;
+	space = size - d_size; //what? why?
+	if (space == 0)
+		return ((size_t)(size + s_size)); //what why?
+	a = d_size;
+	while (*src != '\0' && space-- > 1)
+		dst[a++] = *src++;
+	dst[a] = '\0';
+	return ((size_t)(s_size + d_size));
 }
