236c6e4e00e7146466b0f7fec49c16a5c6933a7b
Rubikhan <sammaddux@gmail.com>
2017-10-12 19:11:19 -0700

    done tweaking ft_strncat.c

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,31 +6,31 @@
 /*   By: smaddux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:27:24 by smaddux           #+#    #+#             */
-/*   Updated: 2017/10/12 15:28:17 by smaddux          ###   ########.fr       */
+/*   Updated: 2017/10/12 19:10:37 by smaddux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*
-**the if statement in the second while loop is sketchy
-**might want to revist it
-*/
 
 char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
 	char	*a;
 	size_t	b;
 
+	if(n == 0)
+		return (s1);
 	a = s1;
 	b = 0;
 	while (*a != '\0')
 		a++;
-	while ((*a++ = *s2++))
+	while ((*a = *s2))
 	{
+		a++;
+		s2++;
 		if (b >= (n - 1))
 			break ;
 		b++;
 	}
+	*a = '\0';
 	return (s1);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: smaddux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:35:46 by smaddux           #+#    #+#             */
-/*   Updated: 2017/10/12 15:36:45 by smaddux          ###   ########.fr       */
+/*   Updated: 2017/10/12 17:26:42 by smaddux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,15 @@ char	*ft_strrchr(const char *s, int c)
 	a = ft_strlen(s);
 	b = 0;
 	z = (char*)s;
-	while (b < a)
+	while (b <= a)
 	{
-		z++;
+
 		if (*z == (char)c)
 		{
 			bool = 1;
 			y = z;
 		}
+		z++;
 		b++;
 	}
 	if (bool == 1)
