c8f9f030ee4505ab142c14cd1d30ea83a21477f1
Rubikhan <sammaddux@gmail.com>
2017-10-13 01:18:41 -0700

    a

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: smaddux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:45:56 by smaddux           #+#    #+#             */
-/*   Updated: 2017/10/13 01:17:14 by smaddux          ###   ########.fr       */
+/*   Updated: 2017/10/13 01:18:27 by smaddux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,31 @@
 
  char    *ft_strtrim(char const *s) 
  { 
-	 
-	 return(s);
 
-/*      size_t        i;  */
-/*      size_t        start;  */
-/*      size_t        end;  */
-/*      char        *str;  */
+      size_t        i;  
+      size_t        start;  
+      size_t        end;  
+      char        *str;  
 
-/*      if (s == NULL)  */
-/*          return (NULL);  */
-/*      i = 0;  */
-/*      while (s[i] && (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'))  */
-/*          i++;  */
-/*      if (!s[i])  */
-/*          return (ft_strnew(0));  */
-/*      start = i;  */
-/*      while (s[i])  */
-/*          i++;  */
-/*      i--;  */
-/*      while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')  */
-/*          i--;  */
-/*      end = i + 1;  */
-/*      str = ft_strnew(end - start);  */
-/*      if (str == NULL)  */
-/*          return (NULL);  */
-/*      ft_strncpy(str, &s[start], end - start);  */
-/*      return (str);  */
+      if (s == NULL)  
+          return (NULL);  
+      i = 0;  
+      while (s[i] && (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'))  
+          i++;  
+      if (!s[i])  
+          return (ft_strnew(0));  
+      start = i;  
+      while (s[i])  
+          i++;  
+      i--;  
+      while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')  
+          i--;  
+      end = i + 1;  
+      str = ft_strnew(end - start);  
+      if (str == NULL)  
+          return (NULL);  
+      ft_strncpy(str, &s[start], end - start);  
+      return (str);  
  } 
 
 /* char		*ft_strtrim(char const *s) */
