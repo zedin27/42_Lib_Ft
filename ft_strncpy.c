a835360e6b819bc04fbf784aa1529eb2997c22e6
Rubikhan <sammaddux@gmail.com>
2017-10-12 21:06:16 -0700

    ft_strncpy.c now handles 0 being passed as third argument (to len)

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: smaddux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:29:52 by smaddux           #+#    #+#             */
-/*   Updated: 2017/10/12 15:31:19 by smaddux          ###   ########.fr       */
+/*   Updated: 2017/10/12 21:04:12 by smaddux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 	size_t	a;
 	size_t	b;
 
+	if(len == 0)
+		return(dst);
 	b = ft_strlen(src);
 	a = 0;
 	save = dst;
