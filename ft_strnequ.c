ae9c5691d465002a0f794c493b1d1f27b1c2fb0e
Rubikhan <sammaddux@gmail.com>
2017-10-12 19:17:34 -0700

    accounted for n = 0 passed as argument to ft_strnequ.c

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: smaddux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/12 15:31:55 by smaddux           #+#    #+#             */
-/*   Updated: 2017/10/12 16:57:25 by smaddux          ###   ########.fr       */
+/*   Updated: 2017/10/12 19:15:21 by smaddux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	ft_strnequ(const char *s1, const char *s2, size_t n)
 
 	if (s1 == NULL || s2 == NULL)
 		return (0);
+	if (n == 0)
+		return (1);
 	a = 0;
 	while (*s1 == *s2 && (*s1 != '\0' || *s2 != '\0'))
 	{
